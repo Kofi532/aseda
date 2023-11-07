df5fb8c21eb5d93c149bb69b321e1d59c6b5b94b
Kofi532 <56702732+Kofi532@users.noreply.github.com>
2023-11-07 10:12:07 +0000

    Add files via upload

--- adom/urls.py
+++ adom/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.music_view, name='music_view'),
+    path('solfa/', views.music_view, name='music_view'),
     path('trigger-action/', views.music_view, name='trigger_action'),
+    path('all/', views.index, name='index'),
+    path('all/base.html/', views.base, name='base'),
+    path('handle-click/', views.handle_click, name='handle_click'),  # Maps /handle-click/ to handle_click view
+    path('previous/', views.previous_view, name='previous_view'),
+
 ]
